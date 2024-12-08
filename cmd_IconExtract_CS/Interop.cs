e43373c8d61a81f18a0d3f05c864956be611803e
Макс Титов <hhit934@gmail.com>
2024-12-09 02:26:25 +0300

    asd

---
+++ cmd_IconExtract_CS/Interop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmd_IconExtract_CS
+{
+    public static class Interop
+    {
+        public const string LibExtern = "CShellExtractor.dll";
+
+
+        // int Export_ExtructIconToFile(const wchar_t* filePath, const wchar_t* outputPath)
+        // CallingConvention = CallingConvention.Cdecl,
+        [DllImport(LibExtern, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl, EntryPoint = "Export_ExtructIconToFile")]
+        public static extern IntPtr ExtructIconToFile(string filePath, string outputPath);
+    }
+}
---
+++ cmd_IconExtract_CS/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+using cmd_IconExtract_CS;
+
+if(!File.Exists(Interop.LibExtern))
+{
+    Console.WriteLine($"File not found! {Path.GetFullPath(Interop.LibExtern)}");
+    return;
+}
+IntPtr status = Interop.ExtructIconToFile(
+               @"C:\Users\UnderKo\Downloads\Uninstall Tool.rar",
+               Path.GetFullPath(@"extruct.png"));
+
+Console.WriteLine(status);
+Console.ReadLine();
